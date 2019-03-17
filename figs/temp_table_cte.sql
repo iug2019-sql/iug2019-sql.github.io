30e2240bf08d2a3fa515672bb4fb061ba906e95a
ray <ray@rvoelke-laptop.plch.net>
2019-03-17 10:04:25 -0400

    more figures and html

---
+++ figs/temp_table_cte.sql
@@ -0,0 +1,21 @@
+﻿CREATE INDEX temp_hold_data_bib_record_id ON temp_hold_data(bib_record_id);
+ANALYZE temp_hold_data;
+-- common table expression WITH clause
+WITH distinct_titles AS (
+	SELECT
+	t.bib_record_id,
+	string_agg(t.pickup_location_code::TEXT, ',') AS pickup_locations,
+	COUNT(*) as count_holds_title
+	FROM
+	temp_hold_data as t
+	GROUP BY
+	t.bib_record_id
+)
+
+SELECT
+d.*
+FROM
+distinct_titles AS d
+ORDER BY
+d.count_holds_title DESC
+;
